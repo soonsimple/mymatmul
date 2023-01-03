3b2024217ac0ea7796a5d3d47bdbe0b762584eb1
soonsimple <sun_chengbo@126.com>
2023-01-03 10:04:02 +0800

    pure funvtion

--- src/mymatmul.f90
+++ src/mymatmul.f90
@@ -1,11 +1,13 @@
 module mymatmul
    implicit none
    integer, parameter :: dp = selected_real_kind(p=15)
+   
    private
 
    public :: dp, mymatmuls1, mymatmuls2, mymatmulf
 
 contains
+
    subroutine mymatmuls1(a, b, c, m, n)
       integer, intent(in) :: m, n
       real(kind=dp), dimension(1:m, 1:n), intent(in) :: a
@@ -32,6 +34,7 @@ contains
 
       m = size(a, 1)
       n = size(a, 2)
+      
       do i = 1, m
          do j = 1, m
             cc(i, j) = 0._dp
@@ -42,23 +45,23 @@ contains
       end do
    end subroutine
 
-   function mymatmulf(a, b)
+   pure function mymatmulf(a, b) result(c)
       integer :: m, n
       integer :: i, j, k
 
       real(kind=dp), dimension(:, :), intent(in) :: a
       real(kind=dp), dimension(:, :), intent(in) :: b
-      real(kind=dp), dimension(1:size(a, 1), 1:size(a, 1)) :: mymatmulf
-      ! real(kind=dp), dimension(:, :) :: mymatmulf
-      ! error: Array 'mymatmulf' cannot have a deferred shape
+      real(kind=dp), dimension(1:size(a, 1), 1:size(a, 1)) :: c
 
       m = size(a, 1)
       n = size(a, 2)
-      mymatmulf = 0._dp
+      
+      c = 0._dp
+      
       do i = 1, m
          do j = 1, m
             do k = 1, n
-               mymatmulf(i, j) = mymatmulf(i, j) + a(i, k)*b(k, j)
+               c(i, j) = c(i, j) + a(i, k)*b(k, j)
             end do
          end do
       end do
